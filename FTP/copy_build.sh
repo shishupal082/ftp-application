526ebe07a721fcd6c1b2d8c79be8bdac280d47eb
shishupal <shishupal082@gmail.com>
2020-08-17 19:08:27 +0530

    version change added

--- FTP/copy_build.sh
+++ FTP/copy_build.sh
@@ -2,7 +2,7 @@
 
 echo [INFO] Copying file to desired location
 
-appVersionDir="../../project/ftp-app/ftp-app-3.0.2/"
+appVersionDir="../../project/ftp-app/ftp-app-3.0.3/"
 configDataDir="../../project/ftp-app/config-files/"
 #savedFilesDir=${appVersionDir}"/saved-files/"
 
--- FTP/src/main/java/com/project/ftp/config/AppConstant.java
+++ FTP/src/main/java/com/project/ftp/config/AppConstant.java
@@ -19,7 +19,7 @@ public class AppConstant {
     public static final String ALLOWED_HEADERS= "Access-Control-Allow-Headers";
     public static final String FILE = "FILE";
     public static final String FOLDER = "FOLDER";
-    public static final String AppVersion = "3.0.2";
+    public static final String AppVersion = "3.0.3";
     public static final String server = "server";
     public static final String DATE_FORMAT = "YYYY-MM-dd";
     public static final String TIME_FORMAT = "HHmmss";
